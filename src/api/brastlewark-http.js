c216c427dc3b519ef3b8033ac8405c3f154090c5
José Luis del Río <jose-luis.delrio.altran@axa-groupsolutions.com>
2019-10-27 00:43:42 +0200

    execFn

--- src/api/brastlewark-http.js
+++ src/api/brastlewark-http.js
@@ -1,46 +1,52 @@
 import axios from "axios";
-
 const BASE_URL = "http://localhost:5000";
 
+var instance = axios.create({
+	baseURL: BASE_URL,
+	timeout: 1000
+});
+
 const getAllGnomes = async () => {
-	const response = await axios.get(`${BASE_URL}/brastlewark`);
+	const response = await instance.get(`${BASE_URL}/brastlewark`);
 	return response.data;
 };
 
 const searchGnomes = async queryString => {
 	if (queryString) {
-		const response = await axios.get(`${BASE_URL}/brastlewark?${queryString}`);
+		const response = await instance.get(
+			`${BASE_URL}/brastlewark?${queryString}`
+		);
 		return response.data;
 	}
 	return await getAllGnomes();
 };
 
 const getGnomeById = async id => {
-	const response = await axios.get(`${BASE_URL}/brastlewark/${id}`);
+	const response = await instance.get(`${BASE_URL}/brastlewark/${id}`);
 	return response.data;
 };
 
 const getGnomesByName = async name => {
-	const response = await axios.get(`${BASE_URL}/brastlewark?name=${name}`);
+	const response = await instance.get(`${BASE_URL}/brastlewark?name=${name}`);
 	return response.data;
 };
 
 const createGnome = async gnome => {
-	const response = await axios.post(`${BASE_URL}/brastlewark`, {
+	const response = await instance.post(`${BASE_URL}/brastlewark`, {
 		...gnome
 	});
 	return response.data;
 };
 
 const updateGnome = async gnome => {
-	const response = await axios.put(`${BASE_URL}/brastlewark/${gnome.id}`, {
+	const response = await instance.put(`${BASE_URL}/brastlewark/${gnome.id}`, {
 		...gnome
 	});
 	return response.data;
 };
 
 const deleteGnome = async id => {
-	const response = await axios.delete(`${BASE_URL}/brastlewark/${id}`);
+	const response = await instance.delete(`${BASE_URL}/brastlewark/${id}`);
 	return response.data;
 };
 
