97bab6dd6671b5d05ee30ffee59f5e190ede63a8
José Luis del Río <jose-luis.delrio.altran@axa-groupsolutions.com>
2019-10-26 22:22:51 +0200

    create db

--- json-server/server.js
+++ json-server/server.js
@@ -1,11 +1,12 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('json-server\\db.json')
+// eslint-disable-next-line no-undef
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("json-server\\db.json");
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 server.listen(5000, () => {
-  console.log('JSON Server is running')
-})
+	console.log("JSON Server is running");
+});
