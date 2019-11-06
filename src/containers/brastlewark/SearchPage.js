0da4f92f03069cb03671fd1f11fd152673efffff
José Luis del Río <jose-luis.delrio.altran@axa-groupsolutions.com>
2019-11-06 21:52:38 +0100

    Paginator

--- src/containers/brastlewark/SearchPage.js
+++ src/containers/brastlewark/SearchPage.js
@@ -4,7 +4,13 @@ import { searchHabitants } from "../../store/actions/searchActions";
 import PageLayout from "../../components/layout/PageLayout";
 import HabitantItem from "../../components/HabitantItem";
 
-import { InputGroup, Form, FormControl, Button } from "react-bootstrap";
+import {
+	InputGroup,
+	Form,
+	FormControl,
+	Button,
+	Pagination
+} from "react-bootstrap";
 
 const SearchPage = ({
 	searchCriteria,
@@ -17,7 +23,8 @@ const SearchPage = ({
 	useEffect(() => {
 		const search = {
 			...searchCriteria,
-			name: searchByName
+			name: searchByName,
+			page: 1
 		};
 		searchHabitants(search);
 		// eslint-disable-next-line react-hooks/exhaustive-deps
@@ -42,6 +49,22 @@ const SearchPage = ({
 		}
 	};
 
+	const numPages = (searchCriteria, total) => {
+		const numPages = Math.trunc(total / searchCriteria.limit);
+		console.log(numPages, numPages * searchCriteria.limit, total);
+		return numPages * searchCriteria.limit === total ? numPages : numPages + 1;
+	};
+
+	const handlePageChanged = page => {
+		const lastPage = numPages(searchCriteria, total);
+		if (page < 0) page = 1;
+		if (page > lastPage) page = lastPage;
+		const search = {
+			...searchCriteria,
+			page
+		};
+		searchHabitants(search);
+	};
 	return (
 		<PageLayout>
 			<div className='container'>
@@ -67,14 +90,39 @@ const SearchPage = ({
 					</div>
 					<div className='col-sm-3'></div>
 				</div>
-				<div className='row'>
-					<div className='col-sm-9'></div>
-					<div className='col-sm-3  d-flex flex-row-reverse'>
-						<span className='m-4'>{total + " habitants found"}</span>
+				<div className='row mt-5'>
+					<div className='col center-block'>
+						<div
+							class='flexBox'
+							style={{
+								display: "flex",
+								flexFlow: "row wrap",
+								justifyContent: "center"
+							}}>
+							{habitantList && (
+								<Pagination>
+									<Pagination.First onClick={() => handlePageChanged(1)} />
+									<Pagination.Prev
+										onClick={() => handlePageChanged(searchCriteria.page - 1)}
+									/>
+									<Pagination.Item>
+										{searchCriteria.page} of {numPages(searchCriteria, total)} (
+										{total} habitants found)
+									</Pagination.Item>
+									<Pagination.Next
+										onClick={() => handlePageChanged(searchCriteria.page + 1)}
+									/>
+									<Pagination.Last
+										onClick={() =>
+											handlePageChanged(numPages(searchCriteria, total))
+										}
+									/>
+								</Pagination>
+							)}
+						</div>
 					</div>
 				</div>
 			</div>
-			<br />
 			{habitantList && (
 				<ul>
 					{habitantList.map(habitant => (
